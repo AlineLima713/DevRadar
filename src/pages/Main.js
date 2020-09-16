eb30de61ecd604a89c3d9965dde9db1421766e16
Aline Lima <alinelima713@gmail.com>
2020-09-16 09:25:38 -0300

    informações do dev implementadas

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -37,6 +37,14 @@ function Main() {
     <MapView initialRegion={currentRegion} style={styles.map}>
       <Marker coordinate={{ latitude: -8.468146, longitude: -35.731846 }}>
         <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/39857752?s=460&u=1e0dd14e0e4b597d28e5b409377fbd2aee1d37a8&v=4' }}/>
+        
+        <Callout>
+          <View style={styles.callout}> 
+            <Text style={styles.devName}>Marcelo Lima</Text>
+            <Text style={styles.devBio}>I am a full stack developer, majoring in Information Systems at the university of Pernambuco (UPE), currently in the 5th semester.</Text>
+            <Text style={styles.devTechs}>ReactJs, React Native, Node.js</Text>
+          </View>
+        </Callout>
       </Marker>
     </MapView>
   );
@@ -53,6 +61,24 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderWidth: 4,
     borderColor: '#FFF'
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
   },
 })
 
